6b36cef95d9db91170c89fe0d748660ce821875c
RaimundoJSoares <raianimes1000@gmail.com>
2022-03-22 00:43:04 -0400

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { ResizeExample} from './observacoes'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
-      <ResizeExample/>
     </ChakraProvider>
   )
 }
